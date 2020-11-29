06c71a157d348d408367365bc00c7ef321e7b767
Louis Leffler <louisleffler@gmail.com>
2020-11-29 22:28:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,17 @@ def game_hash
        :points => 22
        :rebounds => 12
        :assists => 12
-       :steals =>
+       :steals => 3
+       :blocks => 1
+       :slam_dunks => 1 
+      }
+      {:player_name => "Reggie Evans"
+       :number => 30
+       :shoe => 14
+       :points => 12
+       :rebounds => 12
+       :assists =>
+       :steals
        :blocks
        :slam_dunks
       }]
